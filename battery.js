459120cb48e178b2d78ac9083b2172c6e07d8b70
dimitry.kamarnikov <dimitry.kamarnikov@ironsrc.com>
2019-04-09 17:09:43 +0300

    battery status check added

--- battery.js
+++ battery.js
@@ -9,8 +9,8 @@ module.exports = function battery() {
 
       const data = JSON.parse(stdout)
 
-      if (data.percentage < 20)
+      if (data.percentage < 20 && data.plugged !== 'PLUGGED_AC')
         emitonoff.emit('battery', stdout)
     })
-  }, 5 * 60 * 1000)
+  }, 60 * 60 * 1000)
 }
